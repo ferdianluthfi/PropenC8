16f95ca76e9c3f4802bd3ee070e44eb2909cf1b9
ferdianluthfi <luthfi14ferdian@gmail.com>
2019-03-17 00:41:36 +0700

    cobain hapus sesuatu untuk di merge

--- resources/views/luthfi.blade.php
+++ resources/views/luthfi.blade.php
@@ -7,30 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <script src="main.js"></script>
-
-    <!--belom bisa ngestyle dari beda file:( -->
-    <style src="/public/css"></style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm bg-light navbar-light">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-               <a class="nav-link" href="/">TRAYEK</a>
-            </li>
-            <li>
-                <h1>LIIIZZZZZZ</h1>
-            </li>
-            <li>
-                <h1>TAMBAH LIIIZZZZZZ LAGIII</h1>
-            </li>
-            <li>
-                @foreach ($tasks as $task)
-
-                <h1>{{$task->body}}</h1>
-
-                @endforeach
-            </li>
-        </ul>
-    </nav>
+    <h1>LUTHFI GANTENG</h1>
 </body>
 </html>
