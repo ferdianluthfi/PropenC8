8521c4d8497a2e772c5e5fc6ad7574cf07c4309d
jessipertiwi <jessicapertiwi31@gmail.com>
2019-04-05 23:42:26 +0700

    nyicil frontend : tabel kelola lelang dan susunan berkas kelar

--- resources/views/kelolaLelang.blade.php
+++ resources/views/kelolaLelang.blade.php
@@ -1,98 +1,147 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TRAYEK</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="main.js"></script>
-</head>
-<body>
-    <h1>Kelola Berkas Lelang</h1>
-    <table>
-        <tbody>
-            <tr>
-                <td>
-                    Nama Proyek
-                </td>
-                <td>
-                    :   {{ $proyek->projectName }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Alamat Proyek
-                </td>
-                <td>
-                    :   {{ $proyek->projectAddress }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Nama User apa ini
-                </td>
-                <td>
-                    :   {{ $proyek->name }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Nama Perusahaan
-                </td>
-                <td>
-                    :   {{ $proyek->companyName }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Alamat Proyek
-                </td>
-                <td>
-                    :   {{ $proyek->projectAddress }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Deskripsi
-                </td>
-                <td>
-                    :   {{ $proyek->description }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Nilai Proyek
-                </td>
-                <td>
-                    :   {{ $proyek->projectValue }}
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Perkiraan waktu pengerjaan proyek
-                </td>
-                <td>
-                    :   {{ $proyek->estimatedTime }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
+@extends('layouts.layout')
 
+@section ('content')
+@include('layouts.nav')
+<html>
+<body>
+<!-- isinya -->
+<div class="container-fluid card card-detail-proyek">
     <br>
-    <table border="I">
-        <caption>Kelola Berkas Lelang</caption>
-        <thead>
-            <tr>
+    <p class="font-subtitle-1">Detail Proyek</p>
+    <hr>
+    <div>
+        <p class="font-subtitle-2">Detail Proyek {{ $proyek->projectName }}</p>
+        <br>
+    </div>
+    <div class="row ketengahin">
+        <div class="col-sm-7">
+            <div class="card card-info">
+                <div class="row judul">
+                    <div class="col-sm-9 font-subtitle-4">Informasi Umum</div>
+                    <div class="col-sm-1 font-status-approval">DISETUJUI</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5 font-desc-bold">
+                        <ul>
+                            <li><p>Nama Staf Marketing</p></li>
+                            <li><p>Nama Proyek</p></li>
+                            <li><p>Nama Perusahaan</p></li>
+                            <li><p>Nilai Proyek</p></li>
+                            <li><p>Estimasi Waktu Pengerjaan</p></li>
+                            <li><p>Alamat Proyek</p></li>
+                            <li><p>Deskripsi Proyek</p></li>
+                            <li><p>Nilai Proyek</p></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-7 font-desc">
+                        <ul>
+                            <li><p>:   {{ $proyek->name}}<p></li>
+                            <li><p>:   {{ $proyek->projectName}}<p></li>
+                            <li><p>:   {{ $proyek->companyName}}<p></li>
+                            <li><p>:   {{ $proyek->startDate}}<p></li>
+                            <li><p>:   {{ $proyek->endDate}}<p></li>
+                            <li><p>:   {{ $proyek->projectAddress}}<p></li>
+                            <li><p>:   {{ $proyek->description}}<p></li>
+                            <li><p>:   {{ $proyek->projectValue}}<p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="card card-pm">
+                <br>
+                <p class="font-subtitle-5">Project Manager</p>
+            </div>
+        </div>
+    </div>
+<!--    <div class="container" style="padding:5%;">-->
+<!--        <div class="row bigCard">-->
+<!--            <h3 class="col-md-12" style="text-align:center;">Kelola Berkas Lelang</h3>-->
+<!--        </div>-->
+<!--        <table>-->
+<!--            <tbody>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Nama Proyek-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->projectName }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Alamat Proyek-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->projectAddress }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Nama User apa ini-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->name }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Nama Perusahaan-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->companyName }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Alamat Proyek-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->projectAddress }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Deskripsi-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->description }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Nilai Proyek-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->projectValue }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            <tr>-->
+<!--                <td>-->
+<!--                    Perkiraan waktu pengerjaan proyek-->
+<!--                </td>-->
+<!--                <td>-->
+<!--                    :   {{ $proyek->estimatedTime }}-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
+<!--    </div>-->
+
+    <div class="col-sm-9 ketengahin">
+        <table class="table table-bordered table-hover">
+            <caption>Kelola Berkas Lelang</caption>
+            <thead>
+            <tr style="text-align: center">
                 <th>Id</th>
                 <th>Title</th>
                 <th>File Name</th>
-                <th>Created_at</th>
+                <th>Created At</th>
                 <th>Action</th>
             </tr>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody>
             @foreach ($berkass as $object)
             <tr>
                 <td>
@@ -107,22 +156,41 @@
                 <td>
                     {{ $object->created_at->diffForHumans()}}
                 </td>
-                <td>
+                <td style="text-align: center">
                     <a href="{{ Storage::url($object->path) }}" title="View file {{ $object->title }}">
-                        <i class="fa fa-eye"></i>
+                        <span class="glyphicon glyphicon-eye-open"></span>
                     </a>
                     <a href="{{ route('file.response', $object->id) }}" title="Show or download file {{ $object->title }}">
-                        <i class="fa fa-expand fa-fw"></i>
+                        <span class="glyphicon glyphicon-save"></span>
                     </a>
-                    <a href="{{ route('file.download', $object->id) }}" title="Download file {{ $object->title }}">
-                        <i class="fa fa-download fa-fw"></i>
-                    </a>
+<!--                    <a href="{{ route('file.download', $object->id) }}" title="Download file {{ $object->title }}">-->
+<!--                        <i class="fa fa-download fa-fw"></i>-->
+<!--                    </a>-->
                     <a href="/file/{{ $object->id }}/delete" title="Delete file {{ $object->title }}">
-                        <i class="fa fa-trash fa-fw"></i>
+                        <i class="glyphicon glyphicon-trash"></i>
                     </a>
                 </td>
             </tr>
             @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="col-sm-2 ketengahin">
+        <br>
+        <br>
+        <div>
+            @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+            @endif
+        </div>
+        <p>
+            <a href="/file/upload/{{ $proyek->id }}" class="btn btn-primary">Upload File</a>
+        </p>
+    </div>
+
+
             <!-- <tr>
                 <th>
                     <input type="hidden" value="{{ $proyek->id }}">
@@ -143,20 +211,10 @@
                     <button>Tambah Berkas</button>
                 </th>
             </tr> -->
-        </tbody>
-    </table>
+
+
     <br>
     <br>
-    <div>Upload New File</div>
-    <div>
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-    </div>
-    <p>
-        <a href="/file/upload/{{ $proyek->id }}" class="btn btn-primary">Upload File</a>
-    </p>
+
 </body>
 </html>
