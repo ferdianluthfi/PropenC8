aec7126dde83ea1af54ad75340aa106e3260954a
jessipertiwi <jessicapertiwi31@gmail.com>
2019-04-08 19:55:24 +0700

    update isi surat

--- resources/views/template-surat/myPDF-2.blade.php
+++ resources/views/template-surat/myPDF-2.blade.php
@@ -1,12 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Surat Pengajuan Jaminan Bank</title>
+    <title>Surat Penawaran Rekanan</title>
 </head>
 <body>
-<p>Nama Proyek : {{ $projectName }} </p>
-<p>Nama Perusahaan : {{ $comp }} </p>
-<p>Alamat Proyek : {{ $addr }} </p>
-<p>Nilai Proyek: Rp {{ $val }} </p>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="row">
+    <div class="col-sm-6">
+        <table>
+            <tr class="col-sm-3">
+                <td>Nomor</td>
+                <td>:</td>
+            </tr>
+            <tr class="col-sm-3">
+                <td>Perihal</td>
+                <td>:  Pengajuan Surat Jaminan Bank Proyek {{ $projectName }}</td>
+            </tr>
+        </table>
+    </div>
+
+    <br><br>
+    <div class="col-sm-6">
+        <table>
+            <tr class="col-sm-2">
+                <td>Kepada,</td>
+            </tr>
+            <tr class="col-sm-2">
+                <td>Yth.  Humas Bank Mandiri</td>
+            </tr>
+            <tr class="col-sm-2">
+
+                <td>Kantor Cabang UI Depok</td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div>
+    <p>
+        Dengan Hormat,
+        <br><br>
+        &nbsp;&nbsp;&nbsp; Sebagai jaminan rencana pelaksanaan {{ $projectName }}, bersama ini kami mengajukan pengajuan jaminan bank dengan nilai proyek Rp {{ $val }}.
+        <br><br>
+        Demikian pengajuan ini kami sampaikan, atas perhatian dan kerjasama yang baik kami ucapkan terima kasih.
+    </p>
+</div>
+<div class="col-sm-7">
+
+</div>
+<div class="col-sm-5" style="text-align: right">
+    Hormat Kami, <br>
+    PT Nuansa Karisma Djaya <br><br><br><br><br>
+
+
+
+
+    Surjanto Kuswandi<br>
+    Senior Consultant
+</div>
+
+
 </body>
 </html>
