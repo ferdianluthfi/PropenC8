892769cb2143478ce0458646222370c233955465
jessipertiwi <jessicapertiwi31@gmail.com>
2019-05-07 15:33:31 +0700

    bisa nambah assignment ke db

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Users;
 use App\Assignment;
-use Carbon\Carbon;
 
 
 class PenggunaController extends Controller
@@ -33,7 +32,7 @@ class PenggunaController extends Controller
      */
     public function getAvailablePm()//masukin id proyek lewat sini, $proyek_id dr page yg si detail proyek
     {
-        $proyek_id = 1; // nanti integrasi id proyek
+        $proyek_id = 2; // nanti integrasi id proyek
         $assign = DB::table('assignments')->get();
         $pmgrs = DB::table('users')->where('role', 7)->get();
         $choosenPmId = 0;
@@ -42,44 +41,10 @@ class PenggunaController extends Controller
             $choosenPmFromAssignment = DB::table('assignments')->where('proyek_id', $proyek_id)->first();//not the best way to get pm id
             $choosenPmId = $choosenPmFromAssignment->pengguna_id;
         }
-        $count = 0;
-        /*$pm_proyek = DB::table('assignments')
-            ->select('proyek_id', DB::raw('count(*) as total'))
-            ->groupBy('pengguna_id')
-            ->pluck('total','pengguna_id')->all();
-        */
         $pm_proyek = DB::table('assignments')
             ->groupBy('pengguna_id')
             ->selectRaw('count(proyek_id) as total, pengguna_id')->get();
-//        dd($pm_proyek);
-//        foreach ($pmgrs as $pm){
-            //jadi ceritanya dia mesti cek antara assignment sama anu sama anu
-//            $count = Assignment::where('pengguna_id', '=', '$pm->id')->count();
-//            $results = Assignment::select('category', DB::raw('count(*) as total'))
-//            ->groupBy('category')
-//            ->get();
-
-//        foreach($pmgrs as $pm) {
-//            $count = DB::table('assignments')
-//                ->select(DB::raw('count(*) as proyek_count, pengguna_id'))
-//                ->where('pengguna_id', '<>', '$pm->id')
-//                ->groupBy('status')
-//                ->get();
-//            $users = DB::table('users')
-//                ->select(DB::raw('count(*) as user_count, status'))
-//                ->where('status', '<>', 1)
-//                ->groupBy('status')
-//                ->get();
-//            SELECT COUNT(column_name)
-//            FROM table_name
-//            WHERE condition;
-//            foreach ($assign as $assgn) {
-//                if ($assgn->pengguna_id == $pm->id) {
-//                    $count += 1;
-//                }
-//            }
-
-        return view('kelolaPm',compact('assign', 'pmgrs', 'count', 'choosenPmId', 'proyek_id', 'pm_proyek' ));
+        return view('kelolaPm',compact('assign', 'pmgrs', 'choosenPmId', 'proyek_id', 'pm_proyek' ));
 
     }
 
@@ -113,7 +78,7 @@ class PenggunaController extends Controller
                 DB::table('assignments')->where('proyek_id', $proyek_id)->insert([
                     'pengguna_id' => $request->selected,
                     'proyek_id' => $proyek_id,
-                    'assignmentDate' => Carbon::now()->timestamp
+                    'assignmentDate' => DB::raw('now()')
                 ]);
             }
 
